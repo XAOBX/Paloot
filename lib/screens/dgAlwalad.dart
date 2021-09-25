a280d86139414820f24db1392ba78cfe673998f4
XAOBX <ab_ba_12@hotmail.com>
2021-09-25 13:30:40 +0300

    new1

--- lib/screens/dgAlwalad.dart
+++ lib/screens/dgAlwalad.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Wallad extends StatelessWidget {
+class DgAlwallad extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold();
---
+++ lib/screens/ykaber.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Ykaber extends StatelessWidget {
+  const Ykaber({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
